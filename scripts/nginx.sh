6abbc032a3c0f016a9d0934cead8e7f7687ef440
dukerspace <montolsaklor@gmail.com>
2025-11-17 05:40:11 +0700

    fix: nginx script

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -204,19 +204,19 @@ server {
     location / {
         proxy_pass http://frontend_upstream;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \\$http_upgrade;
         proxy_set_header Connection 'upgrade';
-        proxy_set_header Host $host;
+        proxy_set_header Host \\$host;
     }
 
     # Backend API
     location /api {
         proxy_pass http://backend_upstream;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \\$http_upgrade;
         proxy_set_header Connection 'upgrade';
-        proxy_set_header Host $host;
-        proxy_cache_bypass $http_upgrade;
+        proxy_set_header Host \\$host;
+        proxy_cache_bypass \\$http_upgrade;
     }
 }
 EOF
